a0a752ea19bfe3f8672ba4a5da186c660c553ca4
Musab <mmak.lughat@gmail.com>
2025-07-18 04:55:35 +0500

    fix for vercel again

--- blogsummariser/src/utils/db.ts
+++ blogsummariser/src/utils/db.ts
@@ -6,34 +6,14 @@ if (!MONGODB_URI) {
   throw new Error('Please define the MONGODB_URI environment variable');
 }
 
-type MongooseCache = {
-  conn: typeof mongoose | null;
-  promise: Promise<typeof mongoose> | null;
-};
-
-declare global {
-  // eslint-disable-next-line no-var
-  var mongoose: MongooseCache | undefined;
-}
-
-let cached: MongooseCache = global.mongoose ?? { conn: null, promise: null };
-
-if (!global.mongoose) {
-  global.mongoose = cached;
-}
-
 async function dbConnect() {
-  if (cached.conn) return cached.conn;
-
-  if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI, {
-      bufferCommands: false,
-      dbName: "BlogContent",
-    });
+  if (mongoose.connection.readyState >= 1) {
+    return mongoose;
   }
-
-  cached.conn = await cached.promise;
-  return cached.conn;
+  return mongoose.connect(MONGODB_URI, {
+    bufferCommands: false,
+    dbName: "BlogContent",
+  });
 }
 
 export default dbConnect;
